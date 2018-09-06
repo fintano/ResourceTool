1c6b1ffd16e8667ef766926df9c9484f537d9943
fintano <fintano@nate.com>
2018-09-07 02:56:53 +0900

    최초 커밋

---
+++ ResourceToolTests/ExcelReaderTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Office.Interop.Excel;
+using NUnit.Framework;
+using ResourceTool;
+
+namespace ResourceToolTests
+{
+    [TestFixture]
+    public class ExcelReaderTests
+    {   
+        [SetUp]
+        public void Initialize()
+        {
+        }
+
+        [Test]
+        public void Read_GivenFourByFourArrayExcelFile_ReadCells()
+        {
+            ExcelReader excelReader = new ExcelReader(@"D:\HandOfFate2\Excels\Test.xlsx");
+            Worksheet worksheet = excelReader.GetWorkSheet(0);
+            int oneByOne = worksheet.Cells[1, 1];
+            Assert.AreEqual(100, oneByOne);
+        }
+    }
+}
