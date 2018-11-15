8e41f206c94e8acf70f07717ca7fd3aa2ef37364
fintano <fintano@nate.com>
2018-11-16 00:48:37 +0900

    Xsd 수정 기능 추가

--- HOFTools/ViewModel/SchemaDataGridViewModel.cs
+++ HOFTools/ViewModel/SchemaDataGridViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ActiproSoftware.Windows;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
@@ -10,9 +11,31 @@ using System.Windows.Input;
 
 namespace HOFTools
 {
-    class SchemaDataGridViewModel
+    class SchemaDataGridViewModel : ObservableObjectBase
     {
         readonly private string dirPath = Config.DirPath;
+
+        private DataStore store;
+        private TreeNodeModel _root;
+        private XmlTreeNode _selectedItem;
+        private string tableName;
+
+        public TreeNodeModel Root
+        {
+            get { return _root; }
+            set { _root = value; NotifyPropertyChanged("Root"); }
+        }
+
+        public XmlTreeNode SelectedItem
+        {
+            get { return _selectedItem; }
+            set
+            {
+                _selectedItem = value;
+                NotifyPropertyChanged("SelectedItem");
+                ReadXsd();
+            }
+        }
 
         private ObservableCollection<SchemaSpecification> _schema = new ObservableCollection<SchemaSpecification>();
         public ObservableCollection<SchemaSpecification> ColumnSchemas
@@ -29,8 +52,26 @@ namespace HOFTools
             }
         }
 
+        private ICommand _newBtnCommand;
+        public ICommand NewBtnCommand
+        {
+            get
+            {
+                return _newBtnCommand ?? (_newBtnCommand = new CommandHandler(() => New()));
+            }
+        }
+
         public SchemaDataGridViewModel()
         {
+            store = new DataStore(dirPath);
+            Refresh();
+            Init();
+        }
+
+        private void Init()
+        {
+            tableName = "";
+            ColumnSchemas.Clear();
             ColumnSchemas.Add(new SchemaSpecification()
             {
                 ColumnName = "Id",
@@ -39,9 +80,26 @@ namespace HOFTools
             });
         }
 
+        public void ReadXsd()
+        {
+            tableName = SelectedItem.XmlDataTable.TableName;
+            var table = store.DataSet.Tables[tableName];
+
+            ColumnSchemas.Clear();
+
+            foreach(DataColumn column in table.Columns)
+            {
+                var tokens = column.DataType.ToString().Split('.');
+                ColumnSchemas.Add(new SchemaSpecification
+                {
+                    ColumnName = column.ColumnName,
+                    Type = (ColumnType)Enum.Parse(typeof(ColumnType), tokens[1])
+                });
+            }
+        }
+
         private void Save()
         {
-            string tableName = string.Empty;
             while (tableName == "")
             {
                 tableName = Microsoft.VisualBasic.Interaction.InputBox("테이블명을 입력하세요",
@@ -56,9 +114,21 @@ namespace HOFTools
                 dataTable.Columns.Add(new DataColumn(columnSchema.ColumnName, Type.GetType("System." + columnSchema.Type.ToString())));
             }
 
-            DataStore store = new DataStore(dirPath);
             store.WriteXsd(dataTable);
             MessageBox.Show("저장 완료");
+        }
+
+        private void Refresh()
+        {
+            store.ReadXsdAll();
+            TreeCreater treeCreater = new TreeCreater();
+            Root = treeCreater.Create(store.DataSet.Tables);
+            Root.IsExpanded = true;
+        }
+
+        private void New()
+        {
+            Init();
         }
     }
 }
